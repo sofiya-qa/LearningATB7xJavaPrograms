a663b03a77ba9e5eab9cdd64d27f80612632dc33
sofiya-qa <sofiyaansari64@gmail.com>
2024-07-09 16:51:07 +0530

    All files are added

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,9 +1,9 @@
-package ex_07062024;
+package ex_07072024;
 
 public class Lab001 {
 
     public static void main (String[] args)
     {
-        System.out.println("Hello World Sofiya!!");
+        System.out.println("Hello World !!!!");
     }
 }
--- src/ex_07072024/Lab003.java
+++ src/ex_07072024/Lab003.java
@@ -1,7 +1,7 @@
 package ex_07072024;
 
-public class Lab002 {
+public class Lab003 {
     public static void main(String[] args) {
-        System.out.println("Hi");
+
     }
 }
